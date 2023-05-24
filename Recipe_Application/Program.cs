272c58560766598f8193551c3fd97142cf8167c9
Tauriq <Samuelstauriq@gmail.com>
2023-05-24 23:36:52 +0200

    Add project files.

---
+++ Recipe_Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Recipe_Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Repository
+        }
+    }
+}
